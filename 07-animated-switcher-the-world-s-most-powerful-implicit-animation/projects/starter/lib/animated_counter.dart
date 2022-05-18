87d50a5b437212288629dfd35905ac3616273674
Vincent Ngo <vincentvyngo@yahoo.com>
2022-05-18 22:46:57 +0700

    Add initial files for starter project

---
+++ 07-animated-switcher-the-world-s-most-powerful-implicit-animation/projects/starter/lib/animated_counter.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AnimatedCounter extends StatefulWidget {
+  const AnimatedCounter({super.key});
+
+  @override
+  State<AnimatedCounter> createState() => _AnimatedCounterState();
+}
+
+class _AnimatedCounterState extends State<AnimatedCounter> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const Text('Animated Counter');
+  }
+}
