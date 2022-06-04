34ddd284a9c2ddfda9889710ffb210d6a1824717
Vincent Ngo <vincentvyngo@yahoo.com>
2022-06-05 01:05:17 +0700

    update challenge 2 sample

--- 07-animated-switcher-the-world-s-most-powerful-implicit-animation/projects/challenge/challenge_2_final/lib/flashcard.dart
+++ 07-animated-switcher-the-world-s-most-powerful-implicit-animation/projects/challenge/challenge_2_final/lib/flashcard.dart
@@ -23,27 +23,32 @@ class _FlashCardState extends State<FlashCard> {
           isFront = !isFront;
         });
       },
+      // 1
       child: AnimatedSwitcher(
-        duration: const Duration(milliseconds: 400),
+        // 2
+        duration: const Duration(milliseconds: 800),
         transitionBuilder: (child, animation) {
+          // 3
           final rotate = Tween(begin: pi, end: 0.0).animate(animation);
+          // 4
           return AnimatedBuilder(
-            animation: rotate,
-            child: child,
-            builder: (context, child) {
-              final angle = (const Key('front') != widget.key)
-                  ? min(rotate.value, pi / 2)
-                  : rotate.value;
-              return Transform(
-                transform: Matrix4.rotationX(angle),
-                alignment: Alignment.center,
-                child: child,
-              );
-            });
-      },
-      switchInCurve: Curves.easeIn,
-      switchOutCurve: Curves.easeOut,
-      child: isFront ? _cardFront() : _cardBack(),
+              // 5
+              animation: rotate,
+              // 6
+              child: child,
+              // 7
+              builder: (context, child) {
+                // 8
+                final angle = min(rotate.value, pi / 2);
+                // 9
+                return Transform(
+                  transform: Matrix4.rotationX(angle),
+                  alignment: Alignment.center,
+                  child: child,
+                );
+              });
+        },
+        child: isFront ? _cardFront() : _cardBack(),
       ),
     );
   }
@@ -51,42 +56,45 @@ class _FlashCardState extends State<FlashCard> {
   Widget _cardFront() {
     return Container(
       decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: const BorderRadius.all(Radius.circular(12)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.4),
-              spreadRadius: 1,
-              blurRadius: 8,
-              offset: const Offset(0, 3), // changes position of shadow
-            ),
-          ],
-        ),
+        color: Colors.white,
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.4),
+            spreadRadius: 1,
+            blurRadius: 8,
+            offset: const Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
       key: const Key('front'),
-        child: Center(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text(
-          widget.word.name,
-          textAlign: TextAlign.center,
-          style: const TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
-        ),),),);
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            widget.word.name,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
   }
 
   Widget _cardBack() {
     return Container(
       decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: const BorderRadius.all(Radius.circular(12)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.4),
-              spreadRadius: 1,
-              blurRadius: 8,
-              offset: const Offset(0, 3), // changes position of shadow
-            ),
-          ],
-        ),
+        color: Colors.white,
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.4),
+            spreadRadius: 1,
+            blurRadius: 8,
+            offset: const Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
       key: const Key('back'),
       child: Center(
         child: Padding(
@@ -96,6 +104,10 @@ class _FlashCardState extends State<FlashCard> {
             textAlign: TextAlign.center,
             style: const TextStyle(
               fontSize: 18.0,
-            ),),),),);
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
